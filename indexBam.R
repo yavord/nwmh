6998d06f8e7d95f56c319ca250b98e66dc69c35c
yav-d <yavor.d0@gmail.com>
2021-10-01 16:43:20 +0300

    added BAM indexing script

---
+++ indexBam.R
@@ -0,0 +1,26 @@
+library(Rsamtools)
+
+### This script indexes and/or sorts an input BAM file
+
+# bam paths
+inputPath <- "~/projects/RProjects/nwmh/input/"
+inputBam <- "sample.bam"
+bamFull <- paste(inputPath, inputBam, sep="")
+
+# path after sorting
+# inputSort <- "sampleSort"
+# bamSortFull <- paste(inputPath, inputSort, sep="")
+
+# create BamFile object
+bamFile <- BamFile(bamFull)
+
+# sort
+# sortBam(bamFile, bamSortFull)
+# bamSortFull <- paste(bamSortFull,".bam",sep="") # update path to sorted file
+# bamFile <- BamFile(bamSortFull)
+
+# index BAM file
+indexBam(bamFile)
+
+
+
