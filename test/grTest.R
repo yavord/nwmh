95dfa27a5e80513bbf22acae0758518ceadf87e7
yav-d <yavor.d0@gmail.com>
2021-10-01 16:46:19 +0300

    testing/exploring data

---
+++ test/grTest.R
@@ -0,0 +1,16 @@
+library(GenomicRanges)
+
+gr <- GRanges(
+  seqnames = Rle(c("chr1", "chr2", "chr1", "chr3"), c(1, 3, 2, 4)),
+  ranges = IRanges(101:110, end = 111:120, names = head(letters, 10)),
+  strand = Rle(strand(c("-", "+", "*", "+", "-")), c(1, 2, 2, 3, 2)),
+  score = 1:10,
+  GC = seq(1, 0, length=10))
+
+singles <- split(gr, names(gr))
+
+g <- gr[1:3]
+g <- append(g, singles[[10]])
+
+coverage(g)
+
