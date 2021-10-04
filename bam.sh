079ba0b20d1682bbb98974044191bdf532d1ebb8
yav-d <yavor.d0@gmail.com>
2021-10-04 23:23:40 +0300

    vcf wip

---
+++ bam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# script for part 1 
+
+# Conda env name
+ENV="mosdepth"
+
+mkdir -p output/hotspotBed
+mkdir -p output/panelBed
+
+cd coverage/
+# index BAM
+Rscript indexBam.R
+# write hotspot.bed
+Rscript cosmicParser.R
+cd ../
+
+# calculate coverage
+eval "$(conda shell.bash hook)"
+cd output/panelBed
+conda activate $ENV
+mosdepth --by ../../input/panel.bed panelBed ../../input/sample.bam
+cd ../hotspotBed
+conda activate $ENV
+mosdepth --by ../../input/hotspot.bed hotspotBed ../../input/sample.bam
