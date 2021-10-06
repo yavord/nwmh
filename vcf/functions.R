395dcc2d015447165bb04ddfdac4029dac1c3244
yav-d <yavor.d0@gmail.com>
2021-10-06 09:56:27 +0300

    vcf structure

---
+++ vcf/functions.R
@@ -0,0 +1,11 @@
+annotateVariationType <- function(vcfFix) {
+  # check that input is of the right type
+  if(!is.character(vcfFix)) stop("vcfFix is not a character")
+  
+  # 
+  for (i in length(vcfFix[,1])) {
+    
+  }
+  
+  return()
+}
--- vcf/main.R
+++ vcf/main.R
@@ -1,18 +1,18 @@
 library(vcfR)
 library(magrittr)
+source("functions.R")
 
-# file
+# files
 vcfDir <- "../input/"
-
 vcfName <- "sample.vcf"
 metaName <- "meta.txt"
-
 vcfFull <- paste(vcfDir, vcfName, sep="")
 metaFull <-paste(vcfDir, metaName, sep="")
-
+# metadata to append to header
 meta <- read.delim(metaFull, header=F) %>% unlist(use.names=F)
 vcf <- read.vcfR(vcfFull)
 
+# add meta tags of new fields to vcf
 vcf@meta <- c(vcf@meta, meta)
 vcf@meta
 
